f9dc0000ef6ab377627ab3b812dd5e5c3b7c339f
felipon13 <arayafelipe623@gmail.com>
2024-11-16 17:09:26 -0300

    Initial commit

---
+++ tiendaApp/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from tiendaApp.models import Cargo,Departamento,Empleado,Cancha,Cliente,Entrenador,Reserva
+
+# Register your models here.
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ["id","nombre"]
+    
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = ["codigo","nombre"]
+    
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ["run","nombre","paterno","cargo","departamento"]
+
+class CanchaAdmin(admin.ModelAdmin):
+    list_display = ["nombre","descripcion","tipo","capacidad","estado","creado"]
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ["run","nombre","paterno","materno"]
+
+class EntrenadorAdmin(admin.ModelAdmin):
+    list_display = ["run","nombre","paterno","materno"]
+
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ["cancha","horario","fecha"]
+    
+admin.site.register(Cargo,CargoAdmin)
+admin.site.register(Departamento,DepartamentoAdmin)
+admin.site.register(Empleado,EmpleadoAdmin)
+admin.site.register(Cancha,CanchaAdmin)
+admin.site.register(Cliente,ClienteAdmin)
+admin.site.register(Entrenador,EntrenadorAdmin)
+admin.site.register(Reserva,ReservaAdmin)
